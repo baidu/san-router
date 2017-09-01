630dbd57480653060c375270acef432212fa444f
erik <erik168@163.com>
2017-09-01 19:42:00 +0800

    更精准的匹配

--- src/main.js
+++ src/main.js
@@ -232,7 +232,7 @@ export class Router {
         if (typeof rule === 'string') {
             // 没用path-to-regexp，暂时不提供这么多功能支持
             let regText = rule.replace(
-                /\/:([\w-]+)(?=\/|$)/g,
+                /\/:([a-z0-9_-]+)(?=\/|$)/ig,
                 (match, key) => {
                     keys.push(key);
                     return '/([^/\\s]+)';
