0c15e0acd221b3db8055560954f48d21790c3c22
vincentmrlau <413893093@qq.com>
2018-01-18 16:59:49 +0800

    兼容ie6-ie7
    增加一个选择器，兼容ie6-ie7，与san保持一致

---
+++ src/elementSelector.js
@@ -0,0 +1,21 @@
+/*
+*
+* @file 选择器
+* @author vincent lau/413893093@qq.com
+*/
+
+/**
+ * 元素选择器
+ *
+ * @param selector 选择器
+ * @returns {*} 返回元素或者undefined
+ */
+export default function (selector) {
+    let el;
+    if (document.querySelector) {
+        el = document.querySelector(selector);
+    } else {
+        el = document.getElementById(selector.replace(/#/i, ''));
+    }
+    return el;
+}
